1545fbe419149cc54c8e19aeb41cbcc24b4eeaed
jiangxiaoxv <1043479536@qq.com>
2023-11-03 16:04:04 +0800

    feat: 引入axios和配置路由拦截器

---
+++ src/request/instance.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+const baseURL = import.meta.env.VITE_BASE_URL
+
+const instance = axios.create({
+  baseURL, //请求URL前缀
+  timeout: 50000
+})
+
+// 添加请求拦截器
+instance.interceptors.request.use(
+  function (config) {
+    // 设置token请求头
+    // config.headers['token'] = token
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
+// 添加响应拦截器
+instance.interceptors.response.use(
+  function (response) {
+    if (response.status != 200) {
+      console.log(response)
+    }
+    return response.data
+  },
+  function (error) {
+    const data = error.response
+    console.log(data)
+    return Promise.reject(error)
+  }
+)
+export default instance
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import * as VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue')
+  }
+]
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  const loginStatus = true
+  if (to.path != '/login' && !loginStatus) {
+    next({
+      path: '/login',
+      query: { redirect: to.fullPath } // 将跳转的路由path作为参数，登录成功后跳转到该路由
+    })
+  } else {
+    next()
+  }
 })
 
 export default router
